d82ea9c1b11a4a057f1eff5deef0e1ccaabb50d9
Daniel Escalante <d.escalante@uniandes.edu.co>
2023-11-24 17:59:05 -0500

    Cambios al repo

---
+++ places/monitoring/places/views.py
@@ -0,0 +1,19 @@
+import json
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from .models import Place
+
+# Create your views here.
+def PlaceList(request):
+    queryset = Place.objects.all()
+    context = list(queryset.values('id', 'name'))
+    return JsonResponse(context, safe=False)
+
+def PlaceCreate(request):
+    if request.method == 'POST':
+        data = request.body.decode('utf-8')
+        data_json = json.loads(data)
+        place = Place()
+        place.name = data_json["name"]
+        place.save()
+        return HttpResponse("successfully created variable")
